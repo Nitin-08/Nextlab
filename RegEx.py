c2a71a012f0b36bdc47e8b54d14790faa8779471
Nitin-08 <92460713+Nitin-08@users.noreply.github.com>
2022-08-18 13:22:47 +0530

    Update RegEx.py

--- RegEx.py
+++ RegEx.py
@@ -1,38 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[39]:
-
-
 import re
-
-
-# In[40]:
-
-
-txt = '{"orders":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":648},{"id":649},{"id":650},{"id":651},{"id":652},{"id":653}],"errors":[{"code":3,"message":"[PHP Warning #2] count(): Parameter must be an array or an object that implements Countable (153)"}]}"'
-
-
-# In[71]:
-
-
+txt = '{"orders":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":648},{"id":649},{"id":650},{"id":651},{"id":652},{"id":653}],"errors":[{"code":3,"message":"[PHP Warning #2] count(): Parameter must be an array or an object that implements Countable (153)"}]}
 pattern = re.compile(r'[:](\d|\d\d|\d\d\d)[},]')
-
-
-# In[72]:
-
-
 matches = pattern.finditer(txt)
-
-
-# In[73]:
-
 
 for match in matches :
     print(match)
 
 
-# In[ ]:
 
 
 
